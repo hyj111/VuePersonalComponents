3aa001ddc3e0127307e34c315fd67504935c6c57
hyj111 <1069234622@qq.com>
2021-03-01 19:53:02 +0800

    将svg代码整理到icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,28 +1,63 @@
 <template>
-    <!-- 单文件组件 -->
-    <button class="g-button">按钮</button>
+  <!-- 单文件组件 -->
+  <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+		<g-icon v-if="icon" :name="icon"></g-icon>
+    <slot></slot>
+  </button>
 </template>
 <script>
-export default{
-}
+export default {
+  //   props: ["icon", "iconPosition"],
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+			// 属性检查器
+			validator (value) {
+				return value === 'left' || value==='right'
+			}
+    },
+  },
+};
 </script>
 <style lang="scss">
- .g-button {
-      font-size: var(--font-size);
-      height: var(--button-height);
-      padding: 0 1em;
-      border: 1px solid var(--border-color);
-      border-radius: var(--border-radius);
-      background: var(--button-bg);
-      &:hover {
-      border-color: var(--border-color-hover);
+.g-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: -1;
+    margin-right: 0.1em;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
     }
-      &:active {
-      background-color: var(--button-active-bg);
+    > .content {
+      order: 1;
     }
-      &:focus {
-      outline: none;
-    }
+  }
 }
-  
 </style>
