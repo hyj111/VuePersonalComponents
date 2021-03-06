fef8ef018b610bba6bf6ac3a39185e9ffc2650d3
hyj111 <1069234622@qq.com>
2021-03-06 10:45:52 +0800

    row 和col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:'GuluCol',
+  props:{
+    span:[Number,String]
+  }
+}
+</script>
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  width: 50%;
+  background: grey;
+  border: 1px solid red;
+
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n/24) * 100%;
+    }
+  }
+}
+</style>
---
+++ src/row.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+<script>
+</script>
+<style lang="scss" scoped>
+  .row {
+    display: flex;
+  }
+</style>
