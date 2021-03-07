da8303010b7c00ca1cf277d751b797337767b567
hyj111 <1069234622@qq.com>
2021-03-07 21:08:27 +0800

    实现只有一个toast的功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,23 @@
 import Toast from './toast.vue';
+function createToast({Vue,message, propsData}){
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({
+    propsData
+  })
+  toast.$slots.default = [message] //传了一个默认插槽的内容
+  toast.$mount() // 用来挂载我们的扩展
+  // console.log(toast.$el);
+  document.body.appendChild(toast.$el)
+  return toast
+}
+let currentToast
 export default {
   install(Vue) {
     Vue.prototype.$toast = function(message,toastOptions) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData:toastOptions
-      })
-      toast.$slots.default = [message] //传了一个默认插槽的内容
-      toast.$mount() // 用来挂载我们的扩展
-      // console.log(toast.$el);
-      document.body.appendChild(toast.$el)
-    }
+      if(currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({Vue,message,propsData:toastOptions})
+      }
   }
 }
