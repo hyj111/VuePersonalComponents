f9c83d579f8d7dfc7cf1414002ee73916e9efa9c
hyj111 <1069234622@qq.com>
2021-03-07 11:13:53 +0800

    添加sider动画

--- src/layout.vue
+++ src/layout.vue
@@ -27,7 +27,7 @@ export default {
   display: flex;
   flex-grow: 1;
   flex-direction: column;
-  border: 1px solid red;
+  // border: 1px solid red;
   &.hasSider {
   flex-direction: row;
 }
--- src/sider.vue
+++ src/sider.vue
@@ -1,12 +1,42 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visable">
+      <slot></slot>
+      <button @click="visable = flase">close</button>
+    </div>
+  </transition>
 </template>
 <script>
 export default {
-  name:"GuluSider"
-}
+  name: "GuluSider",
+  data() {
+    return {
+      visable: true,
+    };
+  },
+  methods: {
+    enter(){
+      done()
+    },
+    leave(){
+      done()
+    }
+  },
+};
 </script>
 <style lang="scss" scoped>
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active, .slide-leave-active {
+  transition: all .5s;
+}
+.slide-enter, .slide-leave-to /* .slide-leave-active below version 2.1.8 */ {
+  margin-left: -200px;
+}
 </style>
