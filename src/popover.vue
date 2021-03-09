d64e1a0fbca22632367edd66f28e3755bc99ea6e
hyj111 <1069234622@qq.com>
2021-03-09 19:16:57 +0800

    优化代码

--- src/popover.vue
+++ src/popover.vue
@@ -34,24 +34,29 @@ export default {
     positionContent() {
       const { contentWrapper, triggerWrapper } = this.$refs;
       document.body.appendChild(contentWrapper);
-      let { top, left, height, width } = triggerWrapper.getBoundingClientRect();
-      if (this.position === "top") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + window.scrollY + "px";
-      } else if (this.position === "bottom") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + height + window.scrollY + "px";
-      } else if ((this.position === "left")) {
-        let { height: height2 } = contentWrapper.getBoundingClientRect();
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top =
-          top + window.scrollY + (height - height2) + "px";
-      } else if ((this.position === "right")) {
-        contentWrapper.style.left = left + window.scrollX + width + "px";
-        let { height: height2 } = contentWrapper.getBoundingClientRect();
-        contentWrapper.style.top =
-          top + window.scrollY + (height - height2) / 2 + "px";
+      const { top, left, height, width } = triggerWrapper.getBoundingClientRect();
+      const { height: height2 } = contentWrapper.getBoundingClientRect();
+      let positions = {
+        top: {
+          top:top + window.scrollY,
+          left:left + window.scrollX
+        },
+         bottom: {
+          top:top + height + window.scrollY,
+          left: left + window.scrollX
+        },
+         left: {
+          top: top + window.scrollY + (height - height2)/2,
+          left:left + window.scrollX
+        },
+         right: {
+          top: top + window.scrollY + (height - height2)/2,
+          left: left + window.scrollX + width
+        },
       }
+        contentWrapper.style.left = positions[this.position].left+ "px";
+        contentWrapper.style.top =  positions[this.position].top+ "px";
+
     },
     onClickDocument(e) {
       if (
