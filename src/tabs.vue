b6289bb8f9ef7f9097bbc7132fb168dab38905a6
hyj111 <1069234622@qq.com>
2021-03-09 14:37:32 +0800

    重構tabs

--- src/tabs.vue
+++ src/tabs.vue
@@ -4,50 +4,60 @@
   </div>
 </template>
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 export default {
-  name:'GuluTabs',
+  name: "GuluTabs",
   data() {
     return {
-      eventBus: new Vue()
-    }
+      eventBus: new Vue(),
+    };
   },
   props: {
     selected: {
       type: String,
-      require:true
+      require: true,
     },
     // horizontal 横向 vertical纵向
-    direction:{
+    direction: {
       type: String,
-      default:'horizontal',
-      validator (value) {
-        return ['horizontal','vertical'].includes(value)
-      }
-    }
+      default: "horizontal",
+      validator(value) {
+        return ["horizontal", "vertical"].includes(value);
+      },
+    },
   },
-  mounted(){
-    if(this.$children.length ===0) {
-      console.warn('tabs子组件应该是tabs-head和tabs-nav,但你没有写子组件')
-    }
-    this.$children.forEach(vm=>{
-      if(vm.$options.name === 'GuluTabsHead'){
-        vm.$children.forEach(item=>{
-          if(item.$options.name === 'GuluTabsItem' && item.name === this.selected) {
-            this.eventBus.$emit('update:selected',this.selected,item)
-          }
-        })
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console &&
+          console.warn("tabs子组件应该是tabs-head和tabs-nav,但你没有写子组件");
       }
-    })
-    
+    },
+    selectTab(){
+    this.$children.forEach((vm) => {
+      if (vm.$options.name === "GuluTabsHead") {
+        vm.$children.forEach((item) => {
+          if (
+            item.$options.name === "GuluTabsItem" &&
+            item.name === this.selected
+          ) {
+            this.eventBus.$emit("update:selected", this.selected, item);
+          }
+        });
+      }
+    });
+    },
+  },
+  mounted() {
+    this.checkChildren();
+    this.selectTab()
   },
   provide() {
     return {
-      eventBus: this.eventBus
-    }
-  }
-}
+      eventBus: this.eventBus,
+    };
+  },
+};
 </script>
 <style lang="scss" scoped>
-
 </style>
