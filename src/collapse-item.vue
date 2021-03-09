ded02a119d9c72cf4ccada55612ca8bf47a06cf0
hyj111 <1069234622@qq.com>
2021-03-09 20:40:28 +0800

    可以设置默认展开项collapse

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">{{ title }}</div>
+    <div class="title" @click="toggle">{{ title }}</div>
     <div class="content" v-if="open">
       <slot></slot>
     </div>
@@ -8,16 +8,45 @@
 </template>
 <script>
 export default {
+  inject:['eventBus'],
   data() {
     return {
       open:false
     }
+  },
+  mounted() {
+    this.eventBus && this.eventBus.$on('update:selected',name=>{
+      if(name!==this.name) {
+        this.close()
+      }else {
+        this.show()
+      }
+    })
   },
   props: {
     title: {
       type: String,
       require: true,
     },
+    name:{
+      type:String,
+      required:true
+    }
+  },
+  methods: {
+    toggle(){
+      if(this.open) {
+        this.open = false
+      }else {
+        this.eventBus && this.eventBus.$emit('update:selected',this.name)
+      }     
+    },
+    close(){
+      this.open = false
+    },
+    show(){
+      this.open = true
+    }
   },
 };
 </script>
--- src/collapse.vue
+++ src/collapse.vue
@@ -1,17 +1,39 @@
 <template>
-<div class="collapse"><slot></slot></div>
-
+  <div class="collapse"><slot></slot></div>
 </template>
 <script>
+import Vue from "vue";
 export default {
-  
-}
+  name: "GuluCollapse",
+  data() {
+    return {
+      eventBus: new Vue(),
+    };
+  },
+  props: {
+    single: {
+      type:Boolean,
+      default: false,
+    },
+    selected:{
+      type:String
+    }
+  },
+  provide() {
+      return {
+        eventBus: this.eventBus,
+      };
+  },
+  mounted() {
+    this.eventBus.$emit('update:selected', this.selected)
+  },
+};
 </script>
 <style lang="scss" scoped>
-$grey:#ddd;
-$border-radius:4px;
+$grey: #ddd;
+$border-radius: 4px;
 .collapse {
   border: 1px solid $grey;
-  border-radius:$border-radius
+  border-radius: $border-radius;
 }
 </style>
